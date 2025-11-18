65de3940cc26c0d9fca4bd48c0dafd340d9f0c72
Sandeep <virtugrowdigital@gmail.com>
2025-11-18 16:18:24 +0530

    update url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "https://pashupatibackend1.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
